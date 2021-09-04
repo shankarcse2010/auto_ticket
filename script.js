1a6d4253476adfd86a66379c6afaf80ab324f332
Gowrishankar <shankarcse2010@gmail.com>
2021-09-04 15:17:10 +0530

    model path changes

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const imageUpload = document.getElementById('imageUpload')
 const video = document.getElementById('video')
 let faceMatcher;
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('./auto_ticket/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('./auto_ticket/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./auto_ticket/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('./auto_ticket/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('./auto_ticket/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('auto_ticket/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('auto_ticket/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('auto_ticket/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('auto_ticket/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('auto_ticket/models')
 ]).then(start)
 
 async function start() {
